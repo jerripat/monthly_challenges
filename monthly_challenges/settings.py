6cbf127536ea34cf5a9605145c6caec5c28fd966
jerripat <jerripatterson@gmail.com>
2024-02-26 10:44:44 -0600

    Added additional files

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "challenges", # new
+    "challenges",  # new
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -56,7 +56,8 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            BASE_DIR / "templates", # new],
+            BASE_DIR / "templates",  # new]
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
