fcf359b3343425d148f66beb178c91d796a2f181
jerripat <jerripatterson@gmail.com>
2024-02-27 06:08:47 -0600

    Added additional css

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,13 +2,12 @@
 Module docstring describing the purpose of the module.
 """
 
-from django.contrib import admin
 from django.urls import path
 from challenges import views
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    
     path("", views.index),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge"),
